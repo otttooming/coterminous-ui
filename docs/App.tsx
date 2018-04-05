5052c3894a4dba2e2959f57b348dcfc365c6186d
Ott Tooming <OttTooming@gmail.com>
2018-04-05 21:56:01 +0300

    Initial commit

---
+++ docs/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface AppProps {}
+
+export default class App extends React.Component<AppProps, {}> {
+  render() {
+    return (
+      <h1>Components</h1>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./docs/index.tsx",
+
+  output: {
+    filename: "bundle.js"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"],
+
+    alias: {
+      'next-styled': path.join(__dirname, "src")
+    }
+  },
+
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
+    ]
+  },
+
+  devServer: {
+    contentBase: 'docs/'
+  }
+};
+
