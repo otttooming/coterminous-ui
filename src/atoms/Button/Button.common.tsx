65f8a917ff60ee734aee13d913826d02bb5411e8
Ott Tooming <OttTooming@gmail.com>
2019-08-25 19:46:31 +0300

    Refactor Button to a separate common factory for tag logic

---
+++ src/atoms/Button/Button.common.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { styleSystemSpace } from '../../common/styleSystem';
+import { theme } from '../../common/styles/theme';
+import { StyleSystemCommonProps } from '../../common/styleSystem/molecules/common';
+import { StyleSystemSpaceProps } from '../../common/styleSystem/molecules/space';
+import * as styles from './Button.styles';
+
+const Wrapper = styled.button<WrapperProps>`
+  margin: 0;
+  border: 0;
+  cursor: pointer;
+  padding: 0 ${theme.space.m};
+  display: inline-flex;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+  outline: none;
+  min-width: ${theme.space.xl};
+  justify-content: center;
+  font: inherit;
+  height: ${theme.space.xl};
+  line-height: ${theme.space.xl};
+  font-size: ${theme.fontSize.s};
+  font-weight: ${theme.fontWeight.semiBold};
+  user-select: none;
+
+  ${({ variant }) => (variant === 'link' ? styles.link : styles.button)}
+  ${styleSystemSpace}
+`;
+
+interface Props {
+  onClick?: () => void;
+  /**
+   * Externally sourced icon component support
+   */
+  icon?: React.ReactNode;
+}
+
+interface WrapperProps {
+  /**
+   * Add specific `button` or `a` tag styles irrespective of underlying semantic tag.
+   */
+  variant?: 'link' | 'button';
+}
+
+export type ButtonProps = Props &
+  WrapperProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement> &
+  React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  StyleSystemSpaceProps &
+  StyleSystemCommonProps;
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  icon,
+  ...restProps
+}) => {
+  const handleClick = (
+    event: React.SyntheticEvent<HTMLButtonElement | HTMLAnchorElement>,
+  ) => {
+    event.preventDefault();
+
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <Wrapper {...restProps} onClick={handleClick}>
+      {icon && <styles.IconContainer>{icon}</styles.IconContainer>}
+
+      {children}
+    </Wrapper>
+  );
+};
+
+export default Button;
--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,81 +1,15 @@
 import * as React from 'react';
-import styled, { css } from 'styled-components';
-import { styleSystemSpace } from '../../common/styleSystem';
-import { theme } from '../../common/styles/theme';
-import { StyleSystemCommonProps } from '../../common/styleSystem/molecules/common';
-import { StyleSystemSpaceProps } from '../../common/styleSystem/molecules/space';
-import * as styles from './Button.styles';
+import Common from './Button.common';
+import { ButtonProps as CommonProps } from './Button.common';
 
-const Wrapper = styled.button<WrapperProps>`
-  margin: 0;
-  border: 0;
-  cursor: pointer;
-  padding: 0 ${theme.space.m};
-  display: inline-flex;
-  align-items: center;
-  overflow: hidden;
-  position: relative;
-  outline: none;
-  min-width: ${theme.space.xl};
-  justify-content: center;
-  font: inherit;
-  height: ${theme.space.xl};
-  line-height: ${theme.space.xl};
-  font-size: ${theme.fontSize.s};
-  font-weight: ${theme.fontWeight.semiBold};
-  user-select: none;
+export type AnchorElement = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-  ${({ variant }) => (variant === 'link' ? styles.link : styles.button)}
-  ${styleSystemSpace}
-`;
+export type PartialCommonProps = Omit<CommonProps, keyof AnchorElement>;
 
-interface Props {
-  onClick?: () => void;
-  /**
-   * Externally sourced icon component support
-   */
-  icon?: React.ReactNode;
-}
+export type ButtonProps = PartialCommonProps;
 
-interface WrapperProps {
-  /**
-   * Add specific `button` or `a` tag styles irrespective of underlying semantic tag.
-   */
-  variant?: 'link' | 'button';
-}
-
-type PartialAttributes = Omit<
-  React.HTMLAttributes<HTMLButtonElement>,
-  'onClick'
->;
-
-export type ButtonProps = Props &
-  WrapperProps &
-  PartialAttributes &
-  StyleSystemSpaceProps &
-  StyleSystemCommonProps;
-
-const Button: React.FC<ButtonProps> = ({
-  children,
-  onClick,
-  icon,
-  ...restProps
-}) => {
-  const handleClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-
-    if (onClick) {
-      onClick();
-    }
-  };
-
-  return (
-    <Wrapper {...restProps} onClick={handleClick}>
-      {icon && <styles.IconContainer>{icon}</styles.IconContainer>}
-
-      {children}
-    </Wrapper>
-  );
+const Link: React.FC<ButtonProps> = ({ ...restProps }) => {
+  return <Common {...restProps} />;
 };
 
-export default Button;
+export default Link;
